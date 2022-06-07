d639073d6c16960a822e1a22f89639948f6750fd
Anjan Tiwari <anjantiwari1999@gmail.com>
2022-06-07 23:00:25 +0530

    Added Action method demo

---
+++ ActionMethod_Demo/ActionMethod_Demo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionMethod_Demo.Controllers
+{
+    /*Action methods are public methods it returns acationresult objects that 
+     encapsulates the result of executing methods
+    It can't be private,protected and static.
+    they cant be overloaded versions based on parameters
+    We can create multiple action methods in a controller
+     */
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Show()
+        {
+            return " Hello Anjan Tiwari";
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public int StudentID(int id)
+        {
+            return id;
+        }
+    }
+}
